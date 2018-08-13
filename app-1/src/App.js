356ea20fa61a7899bf098501b608bf18da441f7c
Aftab Baig <aftabbaig@Aftabs-MacBook-Air.local>
2018-08-13 15:42:38 -0500

    first app

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -3,16 +3,47 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super(); 
+
+
+    this.state = {
+      message : " "
+    }
+    this.handlechange = this.handlechange.bind(this)
+  }
+  handlechange(value){
+    this.setState({message: value})
+  }
   render() {
     return (
       <div className="App">
+
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <input onChange={e => this.handlechange(e.target.value)} type="text"></input> 
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <img src={logo} className="App-logo2" alt="logo" />
+          <img src={logo} className="App-logo3" alt="logo" />
+          <img src={logo} className="App-logo4" alt="logo" />
+          <img src={logo} className="App-logo5" alt="logo" />
+          <img src={logo} className="App-logo6" alt="logo" />
+          <img src={logo} className="App-logo7" alt="logo" />
+          <img src={logo} className="App-logo8" alt="logo" />
+          <img src={logo} className="App-logo9" alt="logo" />
+          <img src={logo} className="App-logo10" alt="logo" />
+          <img src={logo} className="App-logo11" alt="logo" />
+          <img src={logo} className="App-logo12" alt="logo" />
+
+
+
+
+
+
+
+          <p>{this.state.message}</p>
+          <h2>React Drill #1 </h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        
       </div>
     );
   }
